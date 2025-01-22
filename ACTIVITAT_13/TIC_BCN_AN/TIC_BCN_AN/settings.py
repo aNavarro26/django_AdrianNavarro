339e463c1cce0212dd943742025b270e85c36091
Adrian Navarro <2023_adrian.navarro@iticbcn.cat>
2025-01-22 20:48:54 +0100

    commit 10m abans

--- ACTIVITAT_13/TIC_BCN_AN/TIC_BCN_AN/settings.py
+++ ACTIVITAT_13/TIC_BCN_AN/TIC_BCN_AN/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = "TIC_BCN_AN.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "ITIC_DB",
+        "USER": "ITIC_user",
+        "PASSWORD": "ITIC",
+        "HOST": "localhost",
+        "PORT": "5433",
     }
 }
 
--- ACTIVITAT_13/TIC_BCN_AN/centre/views.py
+++ ACTIVITAT_13/TIC_BCN_AN/centre/views.py
@@ -1,12 +1,49 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
-
-
 def students(request):
-    return HttpResponse("Vista alumnes")
+    students = [
+        {
+            "name": "Roger",
+            "surname1": "Sobrino",
+            "surname2": "García",
+            "email": "roger@example.com",
+            "course": "DAW2A",
+            "modules": "M07, M08",
+        },
+        {
+            "name": "Oriol",
+            "surname1": "Roca",
+            "surname2": "Perez",
+            "email": "oriol@example.com",
+            "course": "DAW2A",
+            "modules": "M07, M09",
+        },
+    ]
+    return render(request, "centre/students.html", {"students": students})
 
 
 def teachers(request):
-    return HttpResponse("Vista de professors")
+    teachers = [
+        {
+            "name": "Josep",
+            "surname1": "Roca",
+            "surname2": "Martinez",
+            "email": "josep@example.com",
+            "course": "DAW2A, DAW1A",
+            "tutor": True,
+            "modules": "M07, M08",
+        },
+        {
+            "name": "Juanma",
+            "surname1": "Biel",
+            "surname2": "Lopez",
+            "email": "juanma@example.com",
+            "course": "DAW2B",
+            "tutor": False,
+            "modules": "M09, M10",
+        },
+    ]
+    return render(request, "centre/teachers.html", {"teachers": teachers})
